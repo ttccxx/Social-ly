13a3c76f204f5ba300f68feebb5747c5ea5972e8
ttccxx <1921144727@qq.com>
2018-12-28 22:03:34 +0800

    创建测试

--- socially_server/CalendarController/tests.py
+++ socially_server/CalendarController/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .views import *
+from DataController.views import *
+
+class CalendarTest(TestCase):
+    def setUp(self):
+        create_user(123)
+
+    def test_create(self):
+        dic = {
+            'sessionKey': 123,
+            'date': '2018-12-28',
+            'time': '21:36',
+            'thing': 'study',
+            'place': 'lib'
+        }
+        self.assertEqual(create(dic), '001')
--- socially_server/socially/settings.py
+++ socially_server/socially/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'CalendarController',
     'Controller',
     'InvitationController',
+    'sslserver',
 ]
 
 MIDDLEWARE = [
